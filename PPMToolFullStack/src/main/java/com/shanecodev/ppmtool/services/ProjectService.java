167f27305591349ec21c5138af30f82cc816f32c
shaneavelino <shanecodev@gmail.com>
2019-01-14 18:27:23 -0500

    Custom Exceptions for Unique Project Identifiers

--- PPMToolFullStack/src/main/java/com/shanecodev/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/shanecodev/ppmtool/services/ProjectService.java
@@ -1,9 +1,11 @@
 package com.shanecodev.ppmtool.services;
 
 import com.shanecodev.ppmtool.domain.Project;
+import com.shanecodev.ppmtool.exceptions.ProjectIdException;
 import com.shanecodev.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Service
 public class ProjectService {
@@ -12,7 +14,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        //Logic
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + " 'already exists");
+        }
     }
 }
