c09a8348619201bb87d2cfb9c080ae12d18e03c6
shaneavelino <shanecodev@gmail.com>
2019-01-14 20:12:24 -0500

    Find Project by Identifier

--- PPMToolFullStack/src/main/java/com/shanecodev/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/shanecodev/ppmtool/services/ProjectService.java
@@ -18,7 +18,19 @@ public class ProjectService {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             return projectRepository.save(project);
         } catch (Exception e) {
-            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + " 'already exists");
+            throw new ProjectIdException("Project ID '"
+                    + project.getProjectIdentifier().toUpperCase()
+                    + " 'already exists");
         }
+    }
+    public Project findProjectByIdentifier(String projectId) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null) {
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
+        }
+
+        return project;
     }
 }
