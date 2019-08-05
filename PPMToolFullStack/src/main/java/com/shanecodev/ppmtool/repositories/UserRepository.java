fb93063b70456eab1910ad187bbf8d25b33f3dca
shaneavelino <shanecodev@gmail.com>
2019-08-04 21:22:36 -0400

    Create User Object, Validation, Repository, Service

---
+++ PPMToolFullStack/src/main/java/com/shanecodev/ppmtool/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.shanecodev.ppmtool.repositories;
+
+import com.shanecodev.ppmtool.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
