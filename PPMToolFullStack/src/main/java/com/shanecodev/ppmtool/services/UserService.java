0f41de01f199d6d559d5e060d246cacbe239989d
shaneavelino <shanecodev@gmail.com>
2019-08-11 12:10:29 -0400

    user registration

--- PPMToolFullStack/src/main/java/com/shanecodev/ppmtool/services/UserService.java
+++ PPMToolFullStack/src/main/java/com/shanecodev/ppmtool/services/UserService.java
@@ -1,6 +1,7 @@
 package com.shanecodev.ppmtool.services;
 
 import com.shanecodev.ppmtool.domain.User;
+import com.shanecodev.ppmtool.exceptions.UsernameAlreadyExistsException;
 import com.shanecodev.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,13 +17,18 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser (User newUser) {
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-        // Username has to be unique (exception)
+        try {
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            // Username has to be unique (exception)
+            newUser.setUsername(newUser.getUsername());
+            // Make sure that password and confirmPassword match
+            // We don't persist or show the confirmPassword
+            return userRepository.save(newUser);
+        }catch (Exception e) {
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+        }
 
-          // Make sure that password and confirmPassword match
-          // We don't persist or show the confirmPassword
-        return userRepository.save(newUser);
     }
 
 }
